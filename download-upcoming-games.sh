023ca87e22ed549f2a31efd486f3c220aaa8e95c
Larry-Hignight <larry.hignight@gmail.com>
2017-08-09 14:01:02 -0700

    Fixed the date format string in download-upcoming-games.sh

--- download-upcoming-games.sh
+++ download-upcoming-games.sh
@@ -1,2 +1,2 @@
 wget https://projects.fivethirtyeight.com/2017-mlb-predictions/games/ \
-     -O upcoming-games/2017-08-08-1207-predictions.html.gz
+     -O `date +upcoming-games/%Y-%m-%d-%H%M-predictions.html.gz`
