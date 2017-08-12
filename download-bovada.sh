ec20cff8bdf51a60d255b277f894c6d6636731ad
Larry-Hignight <larry.hignight@gmail.com>
2017-08-11 17:04:22 -0700

    Scripts to download the moneylines from Bovada and Oddsshark

---
+++ download-bovada.sh
@@ -0,0 +1,2 @@
+wget https://sports.bovada.lv/baseball/mlb \
+     -O `date +bovada/%Y-%m-%d-%H%M-moneyline.html.gz`
---
+++ download-oddsshark.sh
@@ -0,0 +1,2 @@
+wget http://www.oddsshark.com/mlb/odds \
+     -O `date +oddsshark/%Y-%m-%d-%H%M-moneyline.html.gz`
