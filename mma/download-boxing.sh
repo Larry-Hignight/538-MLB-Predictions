ff2726a9e74eb4832d814d96a82dd545b975f7c0
Larry-Hignight <larry.hignight@gmail.com>
2017-09-16 20:42:42 -0700

    Temp MMA and Boxing money lines; UFC Fight Night 116 and Canelo vs GGG.

---
+++ mma/download-boxing.sh
@@ -0,0 +1,2 @@
+wget http://www.oddsshark.com/ufc/odds \
+     -O `date +boxing-%Y-%m-%d-%H%M-money-line.html.gz`
---
+++ mma/download-mma.sh
@@ -0,0 +1,2 @@
+wget http://www.oddsshark.com/ufc/odds \
+     -O `date +mma-%Y-%m-%d-%H%M-money-line.html.gz`
